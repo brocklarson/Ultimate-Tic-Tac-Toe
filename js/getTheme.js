85ee4c3702e981944e9cd8a7a3d0c32ef6188aa2
brocklarson <brock.larson0725@gmail.com>
2021-10-18 07:54:57 -0600

    Fix custom theme default

--- js/getTheme.js
+++ js/getTheme.js
@@ -1,12 +1,12 @@
 const root = document.documentElement;
 getTheme();
 
-function getTheme(){
+function getTheme() {
     //Default theme is theme 3
-    root.style.setProperty('--base-background-color', localStorage.baseBackgroundColor  || 'linear-gradient(#F2D9CB,#33AAA1)');
+    root.style.setProperty('--base-background-color', localStorage.baseBackgroundColor || 'linear-gradient(#F2D9CB,#33AAA1)');
     root.style.setProperty('--button-color', localStorage.buttonColor || '#0E535A');
     root.style.setProperty('--button-text-color', localStorage.buttonTextColor || 'white');
-    root.style.setProperty('--button-border-color', localStorage.buttonBorderColor || 'none');
+    root.style.setProperty('--button-border-color', localStorage.buttonBorderColor || '#0E535A');
     root.style.setProperty('--button-hover-shadow', localStorage.buttonHoverShadow || '0px 0px 10px black inset');
     root.style.setProperty('--large-gameboard-color', localStorage.largeGameboardColor || 'black');
     root.style.setProperty('--small-gameboards-color', localStorage.smallGameboardsColor || 'black');
